a1f66e742968ee3dfad8d68e030e62aa4e82e759
VicSwain <152931791+VicSwain@users.noreply.github.com>
2024-06-07 17:43:53 -0500

    Fixed configuration to be production ready

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import {__dirname} from 'path';
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig (({mode}) => {
+  const env = loadEnv(mode, __dirname, '')
+  return {
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: env.PORT || "localhost:3001",
         secure: false,
         changeOrigin: true
       }
     }
   }
-})
+}})
